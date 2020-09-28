96e238faa30ca46f36c5e7675117418a7907727b
Radomir Ribic <radomir.ribic@gmail.com>
2020-09-28 19:28:36 +0200

    Uradjen zadatak kreiranja tabela closes #5

--- src/main/java/zadaci/Zadatak1KreiranjeTabela.java
+++ src/main/java/zadaci/Zadatak1KreiranjeTabela.java
@@ -2,6 +2,9 @@ package zadaci;
 
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import model.Avion;
+import model.Roba;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -9,7 +12,11 @@ import java.sql.SQLException;
 public class Zadatak1KreiranjeTabela {
     public static void main(String[] args) {
         try(ConnectionSource conn=new JdbcConnectionSource(Konstante.BAZA_URL)){
+            TableUtils.dropTable(conn, Roba.class,true);
+            TableUtils.dropTable(conn, Avion.class,true);
 
+            TableUtils.createTable(conn,Avion.class);
+            TableUtils.createTable(conn,Roba.class);
 
 
 
